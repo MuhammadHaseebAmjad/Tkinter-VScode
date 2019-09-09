0e4a3c28d730c702597f91e0ee1080a925a353b7
Muhammad Haseeb Amjad <shaikhhaseebamjad@yahoo.com>
2019-09-09 10:36:36 +0500

    project add other label and entry

--- hellow.py
+++ hellow.py
@@ -2,12 +2,18 @@ from tkinter import *  # 1-- basic window open
 root = Tk() # 1-- basic window open
 # root.minsize(300,300) # minimum size of the window
 root.geometry("400x400") # adjustable window
-root.resizable(0,0) # window size is fixed now
+# root.resizable(0,0) # window size is fixed now
 
 l1 = Label(root,text = "Enter Name",font = ("Arial",20))
-l1.grid(row = 0,column = 0,pady = 25)
+l1.grid(row = 0,column = 0,pady = 25) #pady for give space
 
 e1 = Entry(root, font = ("Arial",20))
-e1.grid(row = 0,column = 1,pady = 25)
+e1.grid(row = 0,column = 1,pady = 25) #pady for give space
+
+l2 = Label(root,text = "Enter Password",font = ("Arial",20))
+l2.grid(row = 1,column = 0,pady = 25) #pady for give space
+
+e2 = Entry(root, font = ("Arial",20))
+e2.grid(row = 1,column = 1,pady = 25) #pady for give space
 
 root.mainloop()  # 1-- basic window open
