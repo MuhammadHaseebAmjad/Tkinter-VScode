87abb7bbf2955714c71d10635596caf29bf55e0f
Muhammad Haseeb Amjad <shaikhhaseebamjad@yahoo.com>
2019-09-09 08:50:02 +0500

    adjustable size of window

--- hellow.py
+++ hellow.py
@@ -1,6 +1,7 @@
 from tkinter import *  # 1-- basic window open
 root = Tk() # 1-- basic window open
-root.minsize(300,300) # size of the window
+# root.minsize(300,300) # minimum size of the window
+root.geometry("400x400") # adjustable
 un = Label(root,text = "SoftWare") # print label of the window
 un.pack() # label is not printed without pack
 un2 = Label(root, text = "Softwear size change", font = ("Arial",15))
