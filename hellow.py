719626064a2de85fca970e79d8731b2e401db469
Muhammad Haseeb Amjad <shaikhhaseebamjad@yahoo.com>
2019-09-09 09:41:42 +0500

    project add pady

--- hellow.py
+++ hellow.py
@@ -5,8 +5,9 @@ root.geometry("400x400") # adjustable window
 root.resizable(0,0) # window size is fixed now
 
 l1 = Label(root,text = "Enter Name",font = ("Arial",20))
-l1.pack()
+l1.grid(row = 0,column = 0,pady = 25)
 
 e1 = Entry(root, font = ("Arial",20))
-e1.pack()
+e1.grid(row = 0,column = 1,pady = 25)
+
 root.mainloop()  # 1-- basic window open
