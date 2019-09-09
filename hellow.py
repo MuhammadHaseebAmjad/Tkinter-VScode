41ef5c737eacd532336175df9fd1ab4e8e10c37c
Muhammad Haseeb Amjad <shaikhhaseebamjad@yahoo.com>
2019-09-09 09:26:21 +0500

    add Button and adjust font size and color

--- hellow.py
+++ hellow.py
@@ -4,8 +4,8 @@ root = Tk() # 1-- basic window open
 root.geometry("400x400") # adjustable window
 root.resizable(0,0) # window size is fixed now
 
-un = Entry(root,font=("Arial",20),fg = "blue")
-un.pack()
+b1 =Button(root,text = "Click !!!",font = ("Arial",20),fg = "green") 
+b1.pack()
 
 
 root.mainloop()  # 1-- basic window open
