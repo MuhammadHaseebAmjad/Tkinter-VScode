476d3a91e52f9471c6a2edab15e80c7ef91d4932
Muhammad Haseeb Amjad <shaikhhaseebamjad@yahoo.com>
2019-09-09 08:52:04 +0500

    window size fixed

--- hellow.py
+++ hellow.py
@@ -1,7 +1,8 @@
 from tkinter import *  # 1-- basic window open
 root = Tk() # 1-- basic window open
 # root.minsize(300,300) # minimum size of the window
-root.geometry("400x400") # adjustable
+root.geometry("400x400") # adjustable window
+root.resizable(0,0) # window size is fixed now
 un = Label(root,text = "SoftWare") # print label of the window
 un.pack() # label is not printed without pack
 un2 = Label(root, text = "Softwear size change", font = ("Arial",15))
