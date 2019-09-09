fb5e2ce594fa0402c98184236e5469011b3c0ec3
Muhammad Haseeb Amjad <shaikhhaseebamjad@yahoo.com>
2019-09-09 08:44:39 +0500

    adjust window size

--- hellow.py
+++ hellow.py
@@ -1,6 +1,6 @@
 from tkinter import *  # 1-- basic window open
 root = Tk() # 1-- basic window open
-# root.minsize(300,300) # size of the window
+root.minsize(300,300) # size of the window
 un = Label(root,text = "SoftWare") # print label of the window
 un.pack() # label is not printed without pack
 # un2 = Label(root, text = "Softwear size change", font = ("Arial",15))
