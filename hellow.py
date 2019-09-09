4b7670f72f55fbdc2d6459f97156f175440b6394
Muhammad Haseeb Amjad <shaikhhaseebamjad@yahoo.com>
2019-09-09 08:45:43 +0500

    adjust label size and font

--- hellow.py
+++ hellow.py
@@ -3,7 +3,7 @@ root = Tk() # 1-- basic window open
 root.minsize(300,300) # size of the window
 un = Label(root,text = "SoftWare") # print label of the window
 un.pack() # label is not printed without pack
-# un2 = Label(root, text = "Softwear size change", font = ("Arial",15))
-# un2.pack()
+un2 = Label(root, text = "Softwear size change", font = ("Arial",15))
+un2.pack()
 root.mainloop()  # 1-- basic window open
 # print("jaan")
