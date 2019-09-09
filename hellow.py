4e0699a536716511ad5013f1bad236232efce1a7
Muhammad Haseeb Amjad <shaikhhaseebamjad@yahoo.com>
2019-09-09 08:55:32 +0500

    bg and fg color change in label

--- hellow.py
+++ hellow.py
@@ -5,7 +5,7 @@ root.geometry("400x400") # adjustable window
 root.resizable(0,0) # window size is fixed now
 un = Label(root,text = "SoftWare") # print label of the window
 un.pack() # label is not printed without pack
-un2 = Label(root, text = "Softwear size change", font = ("Arial",15))
+un2 = Label(root, text = "Softwear size change", font = ("Arial",15),bg="yellow",fg="white")
 un2.pack()
 root.mainloop()  # 1-- basic window open
 # print("jaan")
